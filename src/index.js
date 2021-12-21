edbf3e564ae3ff866394ec4361812ff2c10f118a
Debas Gebreslasie <65129228+Debas-31@users.noreply.github.com>
2021-12-21 23:52:29 +0300

    Parse data in the popup modal on comment button clicked

--- src/index.js
+++ src/index.js
@@ -5,11 +5,13 @@ const foodsList = document.querySelector('.foods-list');
 const loadFoods = async () => {
   const foods = await getFoods();
   if (foods.meals) {
-    foods.meals.forEach((food) => {
+    foods.meals.forEach((food, index) => {
       foodsList.innerHTML += `
       <!-- Food Card -->
       <div
-        class="card w-3/12 shadow-xl rounded-lg pb-8 overflow-hidden my-10 mx-6" id="${food.idMeal}"
+        class="card w-3/12 shadow-xl rounded-lg pb-8 overflow-hidden my-10 mx-6" id="${
+          food.idMeal
+        }"
       >
         <div class="w-full min-h-64">
           <img
@@ -37,6 +39,7 @@ const loadFoods = async () => {
               text-lg
             "
             id='comment-button'
+            data-set="${index}"
           >
             Comments
           </button>
@@ -53,22 +56,89 @@ const loadFoods = async () => {
   }
 
   const commentBtns = document.querySelectorAll('#comment-button');
-  const closeModal = document.querySelector('.close-modal')
+  const commentModal = document.querySelector('.comment-modal');
 
- 
+  commentBtns.forEach((comment) => {
+    const dataSet = comment.getAttribute('data-set');
 
-  commentBtns.forEach(comment => {
-    comment.addEventListener('click', () => {
-      const commentModal = document.querySelector('.comment-modal')
+    comment.addEventListener('click', async (e) => {
+      const food = foods.meals[dataSet];
+
       commentModal.classList.remove('hidden');
+
+      // Render content on the modal
+      commentModal.innerHTML = `
+        <div class="meal-details w-3/5 my-10 bg-white rounded-lg p-2 z-50 opacity-100">
+          <!-- close btn -->
+          <button type="button" class="float-right mr-3" id="close-modal">
+            <i class="fa fa-close text-2xl hover:text-red-300"></i>
+          </button>
+
+          <!-- Meal contents -->
+          <div class="meal-details-content flex">
+            <div class="w-3/5 pt-2 pb-4 px-12">
+              <div class="recipe-meal-image flex justify-center mt-5">
+                <img
+                  src="${food.strMealThumb}"
+                  alt="Meal name"
+                  class="rounded-lg shadow-lg"
+                  width="480"
+                />
+              </div>
+              <div class="mt-2">
+                <div class="flex justify-between items-center my-4">
+                  <h3 class="text-3xl text-orange-500 mt-5 mb-2">${food.strMeal}</h3>
+                  <div class="flex flex-col justify-center items-center mt-3">
+                    <i class="far fa-heart text-xl hover:text-orange-500"></i>
+                    <span class="mx-3 w-16">5 likes</span>
+                  </div>
+                </div>
+                
+              </div>
+            </div>
+            <div class="w-2/5 p-6">
+              <h3 class="text-2xl text-orange-600">Comments:</h3>
+              <div >
+                <ul class="max-h-64 overflow-y-auto p-3">
+                  <li class="my-2">
+                    <span class="font-semibold">Amadu Kamara: </span>
+                    <span class="text-gray-500">21/12/2021</span> <br />
+                    <span class="text-gray-700 text-sm"
+                      >Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                      Quo, assumenda.</span
+                    >
+                  </li>
+                </ul>
+                <!-- Comment form-->
+                <form class="border p-3 mt-10 rounded">
+                  <h4>Place your comment below</h4>
+                  <input
+                    type="text"
+                    placeholder="Enter your name"
+                    class="border my-2 py-2 px-3 w-full"
+                  />
+                  <textarea
+                    placeholder="Comment..."
+                    id="comment-text"
+                    cols="20"
+                    rows="3"
+                    class="border my-2 py-2 px-3 w-full"
+                  ></textarea>
+                  <button type="submit" class="bg-orange-500 py-2 px-10 w-full text-white rounded-full">Submit</button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      `;
+
+      // Close Modal
+      const closeModal = document.getElementById('close-modal');
       closeModal.addEventListener('click', () => {
         commentModal.classList.add('hidden');
-      })
-    })
-  })
-
+      });
+    });
+  });
 };
 
 loadFoods();
-
-
