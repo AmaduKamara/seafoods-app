65aed6a674f9e4a764892df63c4191e9a2676a01
Amkam <amaduskamara@gmail.com>
2021-12-22 10:15:28 +0000

    Fetch likes from Interactive API and displays on homepage

---
+++ src/displayFoods.js
@@ -0,0 +1,44 @@
+const displayFoods = (food, index, likes) => {
+  return `
+      <!-- Food Card -->
+      <div
+        class="card w-3/12 shadow-xl rounded-lg pb-8 overflow-hidden my-10 mx-6" id="${
+          food.idMeal
+        }"
+      >
+        <div class="w-full min-h-64">
+          <img
+            src="${food.strMealThumb}"
+            alt="Food"
+          />
+        </div>
+        <div class="p-5 my-4 flex justify-between items-center">
+          <h2 class="text-xl text-orange-700">${food.strMeal.slice(0, 24)}</h2>
+          <div class="flex flex-col items-center">
+            <i class="far fa-heart text-xl hover:text-orange-500 like"></i>
+            <span class="mx-3">${likes[index].likes} likes</span>
+          </div>
+          <!-- <i class="fas fa-heart"></i> -->
+        </div>
+        <div class="flex justify-center mt-4">
+          <button
+            class="
+              py-2
+              px-16
+              border border-orange-500
+              rounded-full
+              text-orange-500
+              hover:bg-orange-500 hover:text-orange-100
+              text-lg
+            "
+            id='comment-button'
+            data-set="${index}"
+          >
+            Comments
+          </button>
+        </div>
+      </div>
+    `;
+};
+
+export default displayFoods;
--- src/index.js
+++ src/index.js
@@ -1,52 +1,20 @@
 import getFoods from './utils.js';
+import fetchLikes from './fetchLikes';
+import displayFoods from './displayFoods';
 
 const foodsList = document.querySelector('.foods-list');
 
 const loadFoods = async () => {
   const foods = await getFoods();
 
+  // Fetch likes
+  const likes = await fetchLikes();
+
+  console.log(likes);
+
   if (foods.meals) {
     foods.meals.forEach((food, index) => {
-      foodsList.innerHTML += `
-      <!-- Food Card -->
-      <div
-        class="card w-3/12 shadow-xl rounded-lg pb-8 overflow-hidden my-10 mx-6" id="${
-          food.idMeal
-        }"
-      >
-        <div class="w-full min-h-64">
-          <img
-            src="${food.strMealThumb}"
-            alt="Food"
-          />
-        </div>
-        <div class="p-5 my-4 flex justify-between items-center">
-          <h2 class="text-xl text-orange-700">${food.strMeal.slice(0, 24)}</h2>
-          <div class="flex flex-col items-center">
-            <i class="far fa-heart text-xl hover:text-orange-500"></i>
-            <span class="mx-3">5 likes</span>
-          </div>
-          <!-- <i class="fas fa-heart"></i> -->
-        </div>
-        <div class="flex justify-center mt-4">
-          <button
-            class="
-              py-2
-              px-16
-              border border-orange-500
-              rounded-full
-              text-orange-500
-              hover:bg-orange-500 hover:text-orange-100
-              text-lg
-            "
-            id='comment-button'
-            data-set="${index}"
-          >
-            Comments
-          </button>
-        </div>
-      </div>
-    `;
+      foodsList.innerHTML += displayFoods(food, index, likes);
     });
   } else {
     foodsList.innerHTML += `
@@ -55,6 +23,7 @@ const loadFoods = async () => {
       </div>
     `;
   }
+
 
   const commentBtns = document.querySelectorAll('#comment-button');
   const commentModal = document.querySelector('.comment-modal');
