e494c7d9c21ee3d12c015c55f2f308a7fc51910b
Debas Gebreslasie <65129228+Debas-31@users.noreply.github.com>
2021-12-23 15:56:24 +0300

    Check tes for comment counter

---
+++ src/comment-counter.test.js
@@ -0,0 +1,10 @@
+import commentCounter from "./_mocks_/comment-counter.js";
+
+describe('check for comment counter', ()=> {
+    test('check if the function returns number of comments per item', async()=> {
+        const commentData = await commentCounter(26);
+         const count = commentData.length;
+         expect(expect(count).toEqual(2)
+         )
+    })
+}); 
