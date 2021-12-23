4e630c52f85ae885f211f2a450cc1da138367d22
Amkam <amaduskamara@gmail.com>
2021-12-23 11:49:55 +0000

    Add fixes

--- src/_mocks_/items-counter.js
+++ src/_mocks_/items-counter.js
@@ -2,7 +2,7 @@ import fetch from 'cross-fetch';
 
 const itemsCounter = async () => {
   const response = await fetch(
-    'https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood'
+    'https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood',
   );
   const jsonData = await response.json();
   return jsonData.meals;
